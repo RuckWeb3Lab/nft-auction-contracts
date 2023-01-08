7f31c9c15e76036cf5671febffef3b1dd26a4038
kmasuda <43663858+kmasuda1986@users.noreply.github.com>
2023-01-08 22:39:37 +0900

    Feature/#12 (#13)
    * first commit
    
    * fix: Refactor

--- contracts/NFTAuction.sol
+++ contracts/NFTAuction.sol
@@ -9,7 +9,6 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-// import "hardhat/console.sol";
 
 contract NFTAuction is Context, TimelockController, Ownable2Step, ReentrancyGuard {
     using SafeMath for uint256;
